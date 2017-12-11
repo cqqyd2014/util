29dd75d4aae6e4b23e9e860811e4d1b031f5cbba
cqqyd2014 <cqqyd2014@hotmail.com>
2017-12-11 15:06:20 +0800

    20171211150600

--- src/main/java/com/cqqyd2014/util/Test.java
+++ src/main/java/com/cqqyd2014/util/Test.java
@@ -5,7 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			//System.out.println(com.cqqyd2014.util.DateUtil.getDistanceDateTime(new java.util.Date(),com.cqqyd2014.util.DateUtil.ShortStringToJDate("2009-01-01")));
+			System.out.println(com.cqqyd2014.util.DateUtil.getDistanceChinese(
+					com.cqqyd2014.util.DateUtil.ShortStringToJDate("2016-12-11"),
+					com.cqqyd2014.util.DateUtil.ShortStringToJDate("2016-11-11")));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
